07ccabdec113b9f50ddbd697f5b108a64856b426
Elizabeth <59733938+lmironvish@users.noreply.github.com>
2020-01-15 16:48:55 +0300

    Add files via upload

---
+++ java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends FragmentActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                FragmentB fragb = new FragmentB();
+                ft.add(R.id.holdSpase, fragb);
+                ft.commit();
+            }
+        });
+
+
+    }
+}
